dfd3161d81a886e0bb14e33b713c43c9340f5dfb
BrieCarlson <briecarlson1992@gmail.com>
2026-02-04 04:20:34 -0600

    Demo for Cryptojs

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,14 +1,25 @@
+// Test script using the Page Object Model
 import { test } from "@playwright/test";
-import LoginPage from "../pages/LoginPage";
-
+import LoginPage from "../pages/loginPage";
+import { decrypt, encrypt } from "../utils/CryptojsUtil";
+import { encryptEnvFile, decryptEnvFile } from "../utils/EncryptEnvFile";
 
 test("test", async ({ page }) => {
   const loginPage = new LoginPage(page);
-
   await loginPage.navigateToLoginPage();
-  await loginPage.fillUsername("briecarlson1992.1abd8892032e@agentforce.com");
-  await loginPage.fillPassword("Telina26")
-
+  await loginPage.fillUsername(decrypt(process.env.userid!));
+  await loginPage.fillPassword(decrypt(process.env.password!));
   const homePage = await loginPage.clickLoginButton();
   await homePage.expectServiceTitleToBeVisible();
+});
+
+test.skip("Sample env test", async ({ page }) => {
+  // const plaintext = "Hello, Mars!";
+  // const encryptedText = encrypt(plaintext);
+  // console.log("SALT:", process.env.SALT);
+  // console.log("Encrypted:", encryptedText);
+  // const decryptedText = decrypt(encryptedText);
+  // console.log("Decrypted:", decryptedText);
+  encryptEnvFile();
+  // console.log(decrypt("U2FsdGVkX18dD3Swfx9EAh+j898ItO/RN4dt4DjXXB05oSq3ZZdDPPYeCwS6JfuIA"));
 });
