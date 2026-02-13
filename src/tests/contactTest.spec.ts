f84d6ae2bae531071e9cef4669b2fc2eb4ab148d
BrieCarlson <briecarlson1992@gmail.com>
2026-02-13 17:36:26 -0600

    CICD Check

--- src/tests/contactTest.spec.ts
+++ src/tests/contactTest.spec.ts
@@ -1,5 +1,5 @@
 import { test } from "@playwright/test";
-import LoginPage from "../pages/loginPage";
+import LoginPage from "../pages/LoginPage";
 import { decrypt } from "../utils/CryptojsUtil";
 import logger from "../utils/LoggerUtil";
 import cdata from "../testdata/datademo.json";
